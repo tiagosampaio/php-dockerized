5c28b11ae2160e56b30ad9b97bbc8037efa8310a
Tiago Sampaio <tiago@tiagosampaio.com>
2017-03-23 21:30:27 -0300

    Changes:
    - Added script to install blackfire.io.

---
+++ builders/scripts/blackfire.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget -O - https://packagecloud.io/gpg.key | apt-key add -
+echo "deb http://packages.blackfire.io/debian any main" | tee /etc/apt/sources.list.d/blackfire.list
+apt-get update && apt-get install -y blackfire-php
